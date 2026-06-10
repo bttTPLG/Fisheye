f3514f2f5bcba2556a3a9dc4d0f73defa9a3e1bc
Baptiste COURTADON <b.courtadon@TPLG.vbx>
2026-06-10 10:47:27 +0200

    PhotoCard like

--- src/components/PhotoCard/PhotoCard.jsx
+++ src/components/PhotoCard/PhotoCard.jsx
@@ -1,8 +1,24 @@
+"use client";
+
 import Image from "next/image";
 import "@/styles/photoCard.css";
 import { FaHeart } from "react-icons/fa";
+import { useState } from "react";
 
-export default function PhotoCard({ title = "Titre", counter = "142" }) {
+export default function PhotoCard({ title = "Titre", counter = 14 }) {
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(counter);
+
+  const addLike = () => {
+    if (!liked) {
+      setLikes(likes + 1);
+      setLiked(true);
+    } else {
+      setLikes(likes - 1);
+      setLiked(false);
+    }
+  };
+
   return (
     <article>
       <Image
@@ -15,8 +31,8 @@ export default function PhotoCard({ title = "Titre", counter = "142" }) {
       <div className="title-and-like">
         <p>{title}</p>
         <div className="like">
-          <p className="counter">{counter}</p>
-          <FaHeart className="heart-icon" />
+          <p className="counter">{likes}</p>
+          <FaHeart className="heart-icon" onClick={addLike} />
         </div>
       </div>
     </article>
