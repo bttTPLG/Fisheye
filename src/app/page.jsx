125b52101dc375a07d6ffd8e64db32c0af711b44
Baptiste COURTADON <b.courtadon@TPLG.vbx>
2026-06-10 08:27:18 +0200

    Gallerie components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,19 @@
 import styles from "./page.module.css";
 import Header from "@/components/Header/Header";
 import Card from "@/components/Card/Card";
+import Galerie from "@/components/Galerie/Galerie";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header title="Nos Photographe" />
       <main className={styles.main}>
-        <div className={styles.photographerContainer}>
+        <Galerie>
           <Card />
           <Card />
           <Card />
           <Card />
-        </div>
+        </Galerie>
       </main>
     </div>
   );
---
+++ src/components/Galerie/Galerie.jsx
@@ -0,0 +1,5 @@
+import "@/styles/galerie.css";
+
+export default function Galerie({ children }) {
+  return <section className="gallerie">{children}</section>;
+}
