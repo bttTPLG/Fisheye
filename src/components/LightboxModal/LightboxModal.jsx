2a65fa16da6c840dd219cdf8c87145739ebb3de6
Baptiste COURTADON <b.courtadon@TPLG.vbx>
2026-06-11 08:59:45 +0200

    Correction css pour lightbox

--- src/components/LightboxModal/LightboxModal.jsx
+++ src/components/LightboxModal/LightboxModal.jsx
@@ -5,30 +5,32 @@ import { IoMdClose } from "react-icons/io";
 
 export default function LightboxModal({ title = "Pigeon", onClick }) {
   return (
-    <div className="lightbox-modal">
-      <div className="btn-container">
-        <button className="prev">
-          <FaChevronLeft className="nav-icon" size={30} />
-        </button>
+    <section>
+      <div className="lightbox-modal">
+        <div className="btn-container">
+          <button className="prev">
+            <FaChevronLeft className="nav-icon" size={30} />
+          </button>
+        </div>
+        <div className="photo-and-title">
+          <Image
+            src="/PhotoTest.jpg"
+            width={350}
+            height={300}
+            alt={`Photo ${title}`}
+            className="picture"
+          />
+          <p>{title}</p>
+        </div>
+        <div className="btn-container">
+          <button className="next">
+            <FaChevronRight className="nav-icon" size={30} />
+          </button>
+          <button className="close-lightbox-btn" onClick={onClick}>
+            <IoMdClose size={42} />
+          </button>
+        </div>
       </div>
-      <div className="photo-and-title">
-        <Image
-          src="/PhotoTest.jpg"
-          width={350}
-          height={300}
-          alt={`Photo ${title}`}
-          className="picture"
-        />
-        <p>{title}</p>
-      </div>
-      <div className="btn-container">
-        <button className="next">
-          <FaChevronRight className="nav-icon" size={30} />
-        </button>
-        <button className="close-lightbox-btn" onClick={onClick}>
-          <IoMdClose size={42} />
-        </button>
-      </div>
-    </div>
+    </section>
   );
 }
